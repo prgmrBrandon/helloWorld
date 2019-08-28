b2ff0bf5320ed3312434a067b8ccbacf353cee30
Brandon Haulk <brandonhaulk@gmail.com>
2019-08-28 12:10:38 -0400

    Remove unnecessary comments

--- CS104_IP1_BrandonHaulk.cpp
+++ CS104_IP1_BrandonHaulk.cpp
@@ -1,8 +1,3 @@
-// Unit 1 Sample code
-
-// You must modify it to meet the assignment requirements. No credit will be given if it is submitted without changes
-
-
 #include <iostream>
 
 using namespace std;
