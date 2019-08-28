4b2329ccf125eeae2c0e6190f684708555b84c3a
Brandon Haulk <brandonhaulk@gmail.com>
2019-08-28 12:07:29 -0400

    Hello World program in C++

---
+++ CS104_IP1_BrandonHaulk.cpp
@@ -0,0 +1,18 @@
+// Unit 1 Sample code
+
+// You must modify it to meet the assignment requirements. No credit will be given if it is submitted without changes
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+
+	cout << "Hello World!" << endl; 
+	return 0; 
+
+} 
+
+
